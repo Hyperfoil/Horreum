5051e06c11bdc947f653657bc65499cfd25a94be
Radim Vansa <rvansa@redhat.com>
2020-04-02 11:10:59 +0200

    Delete disfunct tests.

--- repo/src/test/java/io/hyperfoil/tools/NativeExampleResourceIT.java
+++
@@ -1,9 +0,0 @@
-package io.hyperfoil.tools;
-
-import io.quarkus.test.junit.SubstrateTest;
-
-@SubstrateTest
-public class NativeExampleResourceIT extends ExampleResourceTest {
-
-    // Execute the same tests but in native mode.
-}
