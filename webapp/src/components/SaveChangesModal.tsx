51bccde78639d5deaedb65abab8de9923ec2d95d
Radim Vansa <rvansa@redhat.com>
2020-08-25 12:41:24 +0200

    Forgotten (uncommitted) dialog

---
+++ webapp/src/components/SaveChangesModal.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+import {
+    Bullseye,
+    Button,
+    Modal,
+    Spinner,
+} from '@patternfly/react-core'
+
+type SaveChangesModalProps = {
+    isOpen: boolean,
+    onClose(): void
+    onSave?(): Promise<any>,
+    onReset(): void,
+}
+
+export default ({ isOpen, onClose, onSave, onReset} : SaveChangesModalProps) => {
+    const [saving, setSaving] = useState(false)
+    return (
+    <Modal
+        variant="small"
+        title="Save changes?"
+        description="Your changes haven't been saved."
+        showClose={false}
+        isOpen={isOpen}
+    >
+        { saving && <Bullseye><Spinner /></Bullseye> }
+        <Button isDisabled={saving || !onSave} variant="primary" onClick={ () => {
+            if (onSave) {
+                setSaving(true)
+                onSave().finally(() => {
+                    setSaving(false)
+                    onClose()
+                })
+            }
+        }}>Save</Button>
+        <Button isDisabled={saving} variant="secondary" onClick={ onClose }>Cancel</Button>
+        <Button isDisabled={saving} variant="secondary" onClick={ () => {
+            onClose()
+            onReset()
+        }}>Ignore</Button>
+    </Modal>)
+}
