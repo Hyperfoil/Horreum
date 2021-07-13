85d6c9c8317a5e731950d3198d8fee36c033f438
Radim Vansa <rvansa@redhat.com>
2021-07-13 14:41:11 +0200

    Fix testsuite

--- src/test/java/io/hyperfoil/tools/horreum/test/HorreumTestProfile.java
+++ src/test/java/io/hyperfoil/tools/horreum/test/HorreumTestProfile.java
@@ -1,6 +1,5 @@
 package io.hyperfoil.tools.horreum.test;
 
-import java.util.Collections;
 import java.util.Map;
 
 import io.quarkus.test.junit.QuarkusTestProfile;
@@ -8,6 +7,8 @@ import io.quarkus.test.junit.QuarkusTestProfile;
 public class HorreumTestProfile implements QuarkusTestProfile {
    @Override
    public Map<String, String> getConfigOverrides() {
-      return Collections.singletonMap("smallrye.jwt.sign.key-location", "privateKey.jwk");
+      return Map.of(
+            "smallrye.jwt.sign.key-location", "privateKey.jwk",
+            "horreum.url", "http://localhost:8081");
    }
 }
