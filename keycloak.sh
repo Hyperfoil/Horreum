2a9304d012bd979d8ec92a4a02b20ce101effd9a
Radim Vansa <rvansa@redhat.com>
2021-07-13 17:19:14 +0200

    Fix docker-compose

--- keycloak.sh
+++ keycloak.sh
@@ -5,6 +5,10 @@ delete_grafana() {
 }
 trap delete_grafana SIGTERM SIGINT SIGQUIT
 
+if [ $CONTAINER_RUNTIME = "podman" ]; then
+   EXTRA_OPTIONS="-Djboss.bind.address=127.0.0.1 -Djboss.bind.address.private=127.0.0.1"
+fi
+
 delete_grafana
 /opt/jboss/tools/docker-entrypoint.sh \
   -Dkeycloak.profile.feature.upload_scripts=enabled \
@@ -13,5 +17,4 @@ delete_grafana
   -Dkeycloak.migration.file=/etc/keycloak/imports/keycloak-horreum.json \
   -Dkeycloak.migration.strategy=IGNORE_EXISTING \
   -Djboss.socket.binding.port-offset=100 \
-  -Djboss.bind.address=127.0.0.1 \
-  -Djboss.bind.address.private=127.0.0.1
+  $EXTRA_OPTIONS
