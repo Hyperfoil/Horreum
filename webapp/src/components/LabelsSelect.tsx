e5e95ec8035ba3961ad86633cf883c159ecd9257
Radim Vansa <rvansa@redhat.com>
2022-05-18 09:30:56 +0200

    Split LabelsSelect into multiple selects with too many options #198

--- webapp/src/components/LabelsSelect.tsx
+++ webapp/src/components/LabelsSelect.tsx
@@ -2,10 +2,10 @@ import { useEffect, useMemo, useState } from "react"
 import { useSelector } from "react-redux"
 import { teamsSelector } from "../auth"
 
-import { Select, SelectOption, SelectOptionObject } from "@patternfly/react-core"
+import { Select, SelectOption, SelectOptionObject, Split, SplitItem } from "@patternfly/react-core"
 
 import { useDispatch } from "react-redux"
-import { noop } from "../utils"
+import { deepEquals, noop } from "../utils"
 
 export function convertLabels(obj: any): string {
     if (!obj) {
@@ -14,17 +14,29 @@ export function convertLabels(obj: any): string {
         return "<no labels>"
     }
     let str = ""
-    for (let [key, value] of Object.entries(obj)) {
+    for (const [key, value] of Object.entries(obj)) {
         if (str !== "") {
             str = str + ";"
         }
-        if (typeof value === "object") {
-            // Use the same format as Postgres
-            value = JSON.stringify(value).replaceAll(",", ", ").replaceAll(":", ": ")
-        }
-        str = str + key + ":" + value
+        str = str + key + ":" + convertLabelValue(value)
     }
     return str
+}
+
+function convertLabelValue(value: any) {
+    if (typeof value === "object") {
+        // Use the same format as Postgres
+        return JSON.stringify(value).replaceAll(",", ", ").replaceAll(":", ": ")
+    }
+    return value
+}
+
+function convertPartial(value: any) {
+    if (typeof value === "object") {
+        return { ...value, toString: () => convertLabelValue(value) }
+    } else {
+        return value
+    }
 }
 
 export type SelectedLabels = SelectOptionObject | null
@@ -39,8 +51,8 @@ type LabelsSelectProps = {
 }
 
 export default function LabelsSelect(props: LabelsSelectProps) {
-    const [open, setOpen] = useState(false)
     const [availableLabels, setAvailableLabels] = useState<any[]>([])
+    const [partialSelect, setPartialSelect] = useState<any>({})
 
     const dispatch = useDispatch()
     const onSelect = props.onSelect
@@ -67,26 +79,103 @@ export default function LabelsSelect(props: LabelsSelectProps) {
         availableLabels.map(t => ({ ...t, toString: () => convertLabels(t) })).forEach(o => opts.push(o))
         return opts
     }, [availableLabels, all])
+    const filteredOptions = useMemo(() => {
+        return availableLabels.filter(ls => {
+            for (const [key, value] of Object.entries(partialSelect)) {
+                if (typeof value === "object") {
+                    const copy: any = { ...value }
+                    delete copy.toString
+                    if (!deepEquals(ls[key], copy)) {
+                        return false
+                    }
+                } else if (ls[key] !== value) {
+                    return false
+                }
+            }
+            return true
+        })
+    }, [availableLabels, partialSelect])
+    useEffect(() => {
+        if (filteredOptions.length === 1) {
+            props.onSelect(filteredOptions[0])
+        }
+    }, [filteredOptions])
 
     const empty = !options || options.length === 0
     if (empty && !props.showIfNoLabels) {
         return <></>
+    } else if (availableLabels.length < 16) {
+        return (
+            <InnerSelect
+                disabled={props.disabled || empty}
+                all={all}
+                selection={props.selection === null ? all : props.selection}
+                options={options}
+                onSelect={props.onSelect}
+                placeholderText="Choose labels..."
+            />
+        )
+    } else {
+        return (
+            <Split>
+                {[...new Set(availableLabels.flatMap(ls => Object.keys(ls)))].map(key => {
+                    const opts = [...new Set(filteredOptions.map(fo => convertPartial(fo[key])))].sort()
+                    return (
+                        <SplitItem key={key}>
+                            <InnerSelect
+                                disabled={!!props.disabled}
+                                isTypeahead
+                                hasOnlyOneOption={opts.length === 1 && partialSelect[key] === undefined}
+                                selection={opts.length === 1 ? opts[0] : partialSelect[key]}
+                                options={opts}
+                                onSelect={value => {
+                                    const partial = { ...partialSelect }
+                                    if (value !== undefined) {
+                                        partial[key] = value
+                                    } else {
+                                        delete partial[key]
+                                    }
+                                    setPartialSelect(partial)
+                                }}
+                                placeholderText={`Choose ${key}...`}
+                            />
+                        </SplitItem>
+                    )
+                })}
+            </Split>
+        )
     }
-    // TODO: decompose into multiple selects if there are too many options
+}
+
+type InnerSelectProps = {
+    disabled: boolean
+    isTypeahead?: boolean
+    hasOnlyOneOption?: boolean
+    selection?: SelectedLabels
+    options: SelectOptionObject[]
+    all?: SelectOptionObject
+    onSelect(opt: SelectedLabels | undefined): void
+    placeholderText: string
+}
+
+function InnerSelect(props: InnerSelectProps) {
+    const [open, setOpen] = useState(false)
     return (
         <Select
-            isDisabled={props.disabled || empty}
+            isDisabled={props.disabled || props.hasOnlyOneOption}
+            variant={props.isTypeahead ? "typeahead" : "single"}
             isOpen={open}
             onToggle={setOpen}
-            selections={props.selection === null ? all : props.selection}
+            selections={props.selection === null ? props.all : props.selection}
             onSelect={(_, item) => {
-                props.onSelect(item === all ? null : item)
+                props.onSelect(item === props.all ? null : item)
                 setOpen(false)
             }}
+            onClear={props.hasOnlyOneOption ? undefined : () => props.onSelect(undefined)}
             menuAppendTo="parent"
-            placeholderText="Choose labels..."
+            placeholderText={props.placeholderText}
         >
-            {options.map((labels: SelectOptionObject | string, i: number) => (
+            {props.options.map((labels: SelectOptionObject | string, i: number) => (
                 <SelectOption key={i} value={labels} />
             ))}
         </Select>
