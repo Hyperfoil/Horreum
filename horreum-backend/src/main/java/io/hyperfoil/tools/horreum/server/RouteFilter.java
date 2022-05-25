bd1829d829912b92948e5809fdc54fc08a51844d
Radim Vansa <rvansa@redhat.com>
2022-05-25 15:13:06 +0200

    Change RouteFilter implementation not working with Quinoa

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/server/RouteFilter.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/server/RouteFilter.java
@@ -1,37 +1,28 @@
 package io.hyperfoil.tools.horreum.server;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebFilter;
-import javax.servlet.http.HttpFilter;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.enterprise.event.Observes;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
+
+import io.vertx.ext.web.Router;
 
 /*
    Redirects unknown URLs to the default /index.html so that React can try and render them as part of browser Url History
  */
-@WebFilter(filterName = "RouteFilter", asyncSupported = true)
 @ApplicationScoped
-public class RouteFilter extends HttpFilter {
+public class RouteFilter {
    private static final String[] PATH_PREFIXES = { "/api/", "/connect", "/dev" };
    private static final Pattern FILE_NAME_PATTERN = Pattern.compile(".*[.][a-zA-Z\\d]+");
 
-   public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-      chain.doFilter(request, response);
-
-      if (response.getStatus() == 404 && !response.isCommitted()) {
-         String path = request.getRequestURI().substring(
-            request.getContextPath().length()).replaceAll("[/]+$", "");
-         if (Stream.of(PATH_PREFIXES).noneMatch(prefix -> path.startsWith(prefix)) && !FILE_NAME_PATTERN.matcher(path).matches()) {
-            // We could not find the resource, i.e. it is not anything known to the server (i.e. it is not a REST
-            // endpoint or a servlet), and does not look like a file so try handling it in the front-end routes.
-            response.setStatus(200); //force response status when redirecting to /
-            request.getRequestDispatcher("/").forward(request, response);
+   public void init(@Observes Router router) {
+      router.get("/*").handler(rc -> {
+         String path = rc.normalizedPath();
+         if (!path.equals("/") && Stream.of(PATH_PREFIXES).noneMatch(path::startsWith) && !FILE_NAME_PATTERN.matcher(path).matches()) {
+            rc.reroute("/");
+         } else {
+            rc.next();
          }
-      }
+      });
    }
 }
