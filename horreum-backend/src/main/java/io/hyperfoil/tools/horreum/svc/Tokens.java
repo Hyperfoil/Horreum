518482e73d5289a1e5282d4c74f8ccf5f6cca937
Radim Vansa <rvansa@redhat.com>
2022-06-13 10:36:34 +0200

    Use secure (though non-blocking) random source for tokens.

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/Tokens.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/Tokens.java
@@ -1,13 +1,13 @@
 package io.hyperfoil.tools.horreum.svc;
 
-import java.util.concurrent.ThreadLocalRandom;
+import java.security.SecureRandom;
 
 final class Tokens {
+   private static final SecureRandom random = new SecureRandom();
    private Tokens() {}
 
    static String generateToken() {
       StringBuilder sb = new StringBuilder();
-      ThreadLocalRandom random = ThreadLocalRandom.current();
       for (int i = 0; i < 5; ++i) {
          sb.append(String.format("%016x", random.nextLong()));
       }
--- horreum-backend/src/main/resources/horreum.sh
+++ horreum-backend/src/main/resources/horreum.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-JAVA_OPTIONS="-Djava.util.logging.manager=org.jboss.logmanager.LogManager $JAVA_OPTIONS"
+JAVA_OPTIONS="-Djava.util.logging.manager=org.jboss.logmanager.LogManager -Djava.security.egd=file:/dev/urandom $JAVA_OPTIONS"
 if [ -n "$QUARKUS_DATASOURCE_USERNAME" ]; then
   # When the property is set using environment variables it is not correctly propagated
   # to Liquibase changeLog.xml
