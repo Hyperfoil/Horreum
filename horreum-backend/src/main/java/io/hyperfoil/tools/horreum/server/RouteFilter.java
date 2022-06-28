b11375e9d4f0a5b4aa84535194c1bc856734fc7d
Jeremy Whiting <jwhiting@redhat.com>
2022-06-28 13:54:56 +0100

    Added replacement Filter.

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/server/RouteFilter.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/server/RouteFilter.java
@@ -16,7 +16,7 @@ public class RouteFilter {
    private static final Pattern FILE_NAME_PATTERN = Pattern.compile(".*[.][a-zA-Z\\d]+");
 
    public void init(@Observes Router router) {
-      router.get("/*").handler(rc -> {
+      router.get("/*").blockingHandler(rc -> {
          String path = rc.normalizedPath();
          if (!path.equals("/") && Stream.of(PATH_PREFIXES).noneMatch(path::startsWith) && !FILE_NAME_PATTERN.matcher(path).matches()) {
             rc.reroute("/");
