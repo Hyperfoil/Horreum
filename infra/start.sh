4b9fa7e6034960a5770119161ce0db24d9c336f6
jesperpedersen <jesper.pedersen@redhat.com>
2023-02-02 10:09:34 -0500

    Next is 0.7.2

--- infra/start.sh
+++ infra/start.sh
@@ -47,8 +47,8 @@ done
 WORKDIR=$(mktemp -t -d horreum.XXXX)
 echo "Downloading files to $WORKDIR..."
 mkdir -p $WORKDIR/infra
-VERSION=${VERSION:-0.7}
-IMGTAG=${IMGTAG:-0.7}
+VERSION=${VERSION:-0.7.1}
+IMGTAG=${IMGTAG:-0.7.1}
 BASE_URL=https://raw.githubusercontent.com/Hyperfoil/Horreum/${VERSION}
 for FILE in \
   app-init.sh \
