f6d6c063b9232362184f698cbcd3045c5cad2770
jesperpedersen <jesper.pedersen@redhat.com>
2023-03-23 02:10:10 -0400

    Next is 0.7.9

--- horreum-model/src/main/java/io/hyperfoil/tools/horreum/Version.java
+++ horreum-model/src/main/java/io/hyperfoil/tools/horreum/Version.java
@@ -1,5 +1,5 @@
 package io.hyperfoil.tools.horreum;
 
 public class Version {
-   public static final String VERSION = "0.7.8";
+   public static final String VERSION = "0.7.9";
 }
--- infra/start.sh
+++ infra/start.sh
@@ -47,8 +47,8 @@ done
 WORKDIR=$(mktemp -t -d horreum.XXXX)
 echo "Downloading files to $WORKDIR..."
 mkdir -p $WORKDIR/infra
-VERSION=${VERSION:-0.7.7}
-IMGTAG=${IMGTAG:-0.7.7}
+VERSION=${VERSION:-0.7.8}
+IMGTAG=${IMGTAG:-0.7.8}
 BASE_URL=https://raw.githubusercontent.com/Hyperfoil/Horreum/${VERSION}
 for FILE in \
   app-init.sh \
