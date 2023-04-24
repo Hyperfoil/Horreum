0075482095933b88ee0fb11f83aa61506908212e
John OHara <johara@redhat.com>
2023-04-24 17:38:48 +0100

    Ensure rendering of fingerprint options is consistent across all tests - Fixes #481

--- webapp/src/domain/alerting/Changes.tsx
+++ webapp/src/domain/alerting/Changes.tsx
@@ -275,6 +275,7 @@ export default function Changes() {
                                         selection={selectedFingerprint}
                                         onSelect={setSelectedFingerprint}
                                         source={fingerprintSource}
+                                        forceSplit={true}
                                     />
                                 )}
                                 {selectedTest && (
