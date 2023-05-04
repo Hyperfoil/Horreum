acbd1c268e73a77196cb62944c9ece91bd60e82f
John OHara <johara@redhat.com>
2023-05-04 10:21:32 +0100

    Enable Infra on new versions of podman

--- infra/create-db.sh
+++ infra/create-db.sh
@@ -2,11 +2,11 @@
 
 psql -c "SELECT 1;" || exit 1
 if ! psql -t -c "SELECT 1 FROM pg_roles WHERE rolname = 'keycloak';" | grep -q 1; then
-psql -c "CREATE ROLE keycloak noinherit login password 'secret';"
+  psql -c "CREATE ROLE keycloak noinherit login password 'secret';"
 fi
 if ! psql -t -c "SELECT 1 FROM pg_database WHERE datname = 'keycloak';" | grep -q 1; then
-psql -c "CREATE DATABASE keycloak WITH OWNER = 'keycloak';"
+  psql -c "CREATE DATABASE keycloak WITH OWNER = 'keycloak';"
 fi
 if ! psql -t -c "SELECT 1 FROM pg_roles WHERE rolname = 'appuser';" | grep -q 1; then
-    psql -c "CREATE ROLE appuser noinherit login password 'secret';"
+  psql -c "CREATE ROLE appuser noinherit login password 'secret';"
 fi
