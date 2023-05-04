040760090fc014f16d7388598a81cf652d9873b7
John OHara <johara@redhat.com>
2023-05-04 10:21:32 +0100

    Enable Infra on new versions of podman

--- infra/podman-compose.sh
+++ infra/podman-compose.sh
@@ -22,9 +22,6 @@ rm -rf ../horreum-backend/.env ../horreum-integration/target > /dev/null 2>&1
 podman untag horreum_keycloak:latest > /dev/null 2>&1
 
 # Get the definition and filter out "--net horreum_default"
-podman-compose -p horreum -f docker-compose.yml --dry-run up -d | \
-    grep -e '^podman' | \
-    sed 's/--net horreum_default//' | \
-    sed 's/--network-alias [a-zA-Z0-9_-]*//' | \
-    sed "s/\(\[\|]\|\"\)/'\1'/g" | bash
+podman-compose -p horreum -f docker-compose.yml up -d
+
 cd $ORIGINAL_DIR
