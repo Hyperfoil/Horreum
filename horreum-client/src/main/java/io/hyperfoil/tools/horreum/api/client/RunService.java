19ab0d4045a3e4f6bee23e46d2869e971b865dd6
John OHara <johara@redhat.com>
2023-06-08 06:46:39 +0100

    Reinstate workaround for leaking reative types in client

---
+++ horreum-client/src/main/java/io/hyperfoil/tools/horreum/api/client/RunService.java
@@ -0,0 +1,154 @@
+package io.hyperfoil.tools.horreum.api.client;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import io.hyperfoil.tools.horreum.api.data.Access;
+import io.hyperfoil.tools.horreum.api.data.Run;
+import io.hyperfoil.tools.horreum.api.services.QueryResult;
+
+import io.hyperfoil.tools.horreum.api.services.RunService.RunsSummary;
+import io.hyperfoil.tools.horreum.api.services.RunService.RunSummary;
+import io.hyperfoil.tools.horreum.api.services.RunService.RunCount;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * THIS IS A DUPLICATE CLASS specifically for the client.
+ * This class is missing the `addRunFromData` that leaks resteasy reactive types in the API
+ * A Custom implementation for the `addRunFromData` method is included in RunServiceExtension
+ */
+
+@Path("/api/run")
+@Consumes({ MediaType.APPLICATION_JSON})
+@Produces(MediaType.APPLICATION_JSON)
+public interface RunService {
+   @GET
+   @Path("{id}")
+   Object getRun(@PathParam("id") int id,
+                 @QueryParam("token") String token);
+
+   @GET
+   @Path("{id}/summary")
+   RunSummary getRunSummary(@PathParam("id") int id, @QueryParam("token") String token);
+
+   @GET
+   @Path("{id}/data")
+   Object getData(@PathParam("id") int id, @QueryParam("token") String token, @QueryParam("schemaUri") String schemaUri);
+
+   @GET
+   @Path("{id}/metadata")
+   Object getMetadata(@PathParam("id") int id, @QueryParam("token") String token, @QueryParam("schemaUri") String schemaUri);
+
+   @GET
+   @Path("{id}/query")
+   QueryResult queryData(@PathParam("id") int id,
+                         @QueryParam("query") String jsonpath,
+                         @QueryParam("uri") String schemaUri,
+                         @QueryParam("array") @DefaultValue("false") boolean array);
+
+   @POST
+   @Path("{id}/resetToken")
+   String resetToken(@PathParam("id") int id);
+
+   @POST
+   @Path("{id}/dropToken")
+   String dropToken(@PathParam("id") int id);
+
+   @POST
+   @Path("{id}/updateAccess")
+   // TODO: it would be nicer to use @FormParams but fetchival on client side doesn't support that
+   void updateAccess(@PathParam("id") int id,
+                     @QueryParam("owner") String owner,
+                     @QueryParam("access") Access access);
+
+   @POST
+   @Path("test/{test}")
+   @Consumes(MediaType.APPLICATION_JSON)
+   Response add(@PathParam("test") String testNameOrId,
+              @QueryParam("owner") String owner,
+              @QueryParam("access") Access access,
+              @QueryParam("token") String token,
+              Run run);
+
+   @POST
+   @Path("data")
+   @Produces(MediaType.TEXT_PLAIN) // run ID as string
+   Response addRunFromData(@QueryParam("start") String start,
+                         @QueryParam("stop") String stop,
+                         @QueryParam("test") String test,
+                         @QueryParam("owner") String owner,
+                         @QueryParam("access") Access access,
+                         @QueryParam("token") String token,
+                         @QueryParam("schema") String schemaUri,
+                         @QueryParam("description") String description,
+                         JsonNode data);
+
+   @GET
+   @Path("autocomplete")
+   List<String> autocomplete(@QueryParam("query") String query);
+
+   @GET
+   @Path("list")
+   RunsSummary listAllRuns(@QueryParam("query") String query,
+                           @QueryParam("matchAll") boolean matchAll,
+                           @QueryParam("roles") String roles,
+                           @QueryParam("trashed") boolean trashed,
+                           @QueryParam("limit") Integer limit,
+                           @QueryParam("page") Integer page,
+                           @QueryParam("sort") String sort,
+                           @QueryParam("direction") String direction);
+
+   @GET
+   @Path("count")
+   RunCount runCount(@QueryParam("testId") int testId);
+
+   @GET
+   @Path("list/{testId}")
+   RunsSummary listTestRuns(@PathParam("testId") int testId,
+                                                                               @QueryParam("trashed") boolean trashed,
+                                                                               @QueryParam("limit") Integer limit,
+                                                                               @QueryParam("page") Integer page,
+                                                                               @QueryParam("sort") String sort,
+                                                                               @QueryParam("direction") String direction);
+
+   @GET
+   @Path("bySchema")
+   RunsSummary listBySchema(@QueryParam("uri") String uri,
+                            @QueryParam("limit") Integer limit,
+                            @QueryParam("page") Integer page,
+                            @QueryParam("sort") String sort,
+                            @QueryParam("direction") String direction);
+
+   @POST
+   @Path("{id}/trash")
+   void trash(@PathParam("id") int id, @QueryParam("isTrashed") Boolean isTrashed);
+
+   @POST
+   @Path("{id}/description")
+   @Consumes(MediaType.TEXT_PLAIN)
+   void updateDescription(@PathParam("id") int id, String description);
+
+   @POST
+   @Path("{id}/schema")
+   @Consumes(MediaType.TEXT_PLAIN)
+   Map<Integer, String> updateSchema(@PathParam("id") int id, @QueryParam("path") String path, String schemaUri);
+
+   @POST
+   @Path("{id}/recalculate")
+   List<Integer> recalculateDatasets(@PathParam("id") int runId);
+
+   @POST
+   @Path("recalculateAll")
+   void recalculateAll(@QueryParam("from") String from, @QueryParam("to") String to);
+
+}
