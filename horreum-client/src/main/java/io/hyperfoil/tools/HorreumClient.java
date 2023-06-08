5b022a2f16ff0ee55ded074a60e3257b88442002
John OHara <johara@redhat.com>
2023-06-08 19:46:54 +0100

    Bootstrap horreum client from remote instance

--- horreum-client/src/main/java/io/hyperfoil/tools/HorreumClient.java
+++ horreum-client/src/main/java/io/hyperfoil/tools/HorreumClient.java
@@ -1,5 +1,6 @@
 package io.hyperfoil.tools;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.hyperfoil.tools.auth.KeycloakClientRequestFilter;
 import io.hyperfoil.tools.horreum.api.client.RunService;
 import io.hyperfoil.tools.horreum.api.services.ActionService;
@@ -8,6 +9,7 @@ import io.hyperfoil.tools.horreum.api.services.BannerService;
 import io.hyperfoil.tools.horreum.api.services.ChangesService;
 import io.hyperfoil.tools.horreum.api.services.ConfigService;
 import io.hyperfoil.tools.horreum.api.services.DatasetService;
+import io.hyperfoil.tools.horreum.api.services.ExperimentService;
 import io.hyperfoil.tools.horreum.api.services.NotificationService;
 import io.hyperfoil.tools.horreum.api.services.ReportService;
 import io.hyperfoil.tools.horreum.api.services.SchemaService;
@@ -29,6 +31,7 @@ import java.io.Closeable;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
@@ -37,14 +40,17 @@ import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 
+import static io.hyperfoil.tools.horreum.api.services.ConfigService.KEYCLOAK_BOOTSTRAP_URL;
+
 public class HorreumClient implements Closeable {
     private final ResteasyClient client;
     public final ActionService actionService;
     public final AlertingService alertingService;
     public final BannerService bannerService;
-    public final DatasetService datasetService;
     public final ChangesService changesService;
     public final ConfigService configService;
+    public final DatasetService datasetService;
+    public final ExperimentService experimentService;
     public final NotificationService notificationService;
     public final ReportService reportService;
     public final RunServiceExtension runService;
@@ -56,14 +62,16 @@ public class HorreumClient implements Closeable {
 
     private HorreumClient(ResteasyClient client,
                           ActionService actionService, AlertingService alertingService, BannerService bannerService, ChangesService changesService, ConfigService configService,
-                          DatasetService datasetService, NotificationService notificationService, ReportService reportService, RunServiceExtension runServiceExtension, SchemaService schemaService,
+                          DatasetService datasetService, ExperimentService experimentService, NotificationService notificationService,
+                          ReportService reportService, RunServiceExtension runServiceExtension, SchemaService schemaService,
                           SqlService sqlService, SubscriptionService subscriptionService, TestService horreumTestService, UserService userService) {
         this.client = client;
         this.alertingService = alertingService;
         this.bannerService = bannerService;
-        this.datasetService = datasetService;
         this.changesService = changesService;
         this.configService = configService;
+        this.datasetService = datasetService;
+        this.experimentService = experimentService;
         this.actionService = actionService;
         this.notificationService = notificationService;
         this.reportService = reportService;
@@ -82,12 +90,8 @@ public class HorreumClient implements Closeable {
 
     public static class Builder {
         private String horreumUrl;
-        private String keycloakUrl;
-        private String keycloakRealm = "horreum";
         private String horreumUser;
         private String horreumPassword;
-        private String clientId = "horreum-ui";
-        private String clientSecret;
         private SSLContext sslContext;
 
         public Builder() {
@@ -98,16 +102,6 @@ public class HorreumClient implements Closeable {
             return this;
         }
 
-        public Builder keycloakUrl(String keycloakUrl) {
-            this.keycloakUrl = keycloakUrl;
-            return this;
-        }
-
-        public Builder keycloakRealm(String keycloakRealm) {
-            this.keycloakRealm = keycloakRealm;
-            return this;
-        }
-
         public Builder horreumUser(String horreumUser) {
             this.horreumUser = horreumUser;
             return this;
@@ -115,16 +109,6 @@ public class HorreumClient implements Closeable {
 
         public Builder horreumPassword(String horreumPassword) {
             this.horreumPassword = horreumPassword;
-            return this;
-        }
-
-        public Builder clientId(String clientId) {
-            this.clientId = clientId;
-            return this;
-        }
-
-        public Builder clientSecret(String clientSecret) {
-            this.clientSecret = clientSecret;
             return this;
         }
 
@@ -164,12 +148,21 @@ public class HorreumClient implements Closeable {
                 }
             }
 
-            KeycloakClientRequestFilter requestFilter = new KeycloakClientRequestFilter(keycloakUrl,
-                    keycloakRealm,
+            ConfigService.KeycloakConfig keycloakConfig;
+            URL url = null;
+            try {
+                url = new URL(this.horreumUrl.concat(KEYCLOAK_BOOTSTRAP_URL));
+                ObjectMapper objectMapper = new ObjectMapper();
+                keycloakConfig = objectMapper.readValue(url, ConfigService.KeycloakConfig.class);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            KeycloakClientRequestFilter requestFilter = new KeycloakClientRequestFilter(keycloakConfig.url,
+                    keycloakConfig.realm,
                     horreumUser,
                     horreumPassword,
-                    clientId,
-                    clientSecret,
+                    keycloakConfig.clientId,
                     sslContext);
 
             ResteasyClientBuilderImpl clientBuilder = new ResteasyClientBuilderImpl();
@@ -194,6 +187,7 @@ public class HorreumClient implements Closeable {
                   target.proxyBuilder(ChangesService.class).build(),
                   target.proxyBuilder(ConfigService.class).build(),
                   target.proxyBuilder(DatasetService.class).build(),
+                  target.proxyBuilder(ExperimentService.class).build(),
                   target.proxyBuilder(NotificationService.class).build(),
                   target.proxyBuilder(ReportService.class).build(),
                   new RunServiceExtension(target, target.proxyBuilder(RunService.class).build()),
