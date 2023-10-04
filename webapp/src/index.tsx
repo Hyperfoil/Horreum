585b26b4ea148770df1712926819756d8ff6dff1
jesperpedersen <jesper.pedersen@redhat.com>
2023-10-04 15:19:17 -0400

    Revert "migrate to createRoot #717"
    This reverts commit 6c894683d84d8eb88c4f33587d4d66536b529f65.

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,18 +1,12 @@
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom"
 
 import "./index.css"
 import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 
 import jsonpath from "jsonpath"
-import React from "react"
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-)
+ReactDOM.render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
