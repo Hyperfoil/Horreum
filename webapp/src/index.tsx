c82b8d937fa21d7d59245a9efb973149e6a3be5b
jesperpedersen <jesper.pedersen@redhat.com>
2023-10-05 08:21:26 -0400

    [#717] Use createRoot

--- webapp/src/index.tsx
+++ webapp/src/index.tsx
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom"
+import ReactDOM from "react-dom/client"
 
 import "./index.css"
 import App from "./App"
@@ -6,7 +6,10 @@ import * as serviceWorker from "./serviceWorker"
 
 import jsonpath from "jsonpath"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+root.render(
+    <App />
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
