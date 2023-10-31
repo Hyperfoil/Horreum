271d690303e2fb827b7bc446159fe423f322f420
John OHara <johara@redhat.com>
2023-10-31 13:58:53 +0000

    Define sequences and ID generators for each PeristentLog subclasses: Fixes #854

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/entity/ActionLogDAO.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/entity/ActionLogDAO.java
@@ -1,10 +1,27 @@
 package io.hyperfoil.tools.horreum.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.id.enhanced.SequenceStyleGenerator;
 
 @Entity(name = "ActionLog")
 public class ActionLogDAO extends PersistentLogDAO {
+
+   @Id
+   @GenericGenerator(
+           name = "actionlog_id_generator",
+           strategy = "io.hyperfoil.tools.horreum.entity.SeqIdGenerator",
+           parameters = {
+                   @org.hibernate.annotations.Parameter(name = SequenceStyleGenerator.INCREMENT_PARAM, value = "1"),
+           }
+   )
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "actionlog_id_generator")
+   public Long id;
+
    @NotNull
    public int testId;
 
