7061df829ccf6dd8f284c3a11e40e73ec60430db
John OHara <johara@redhat.com>
2023-10-31 13:58:53 +0000

    Define sequences and ID generators for each PeristentLog subclasses: Fixes #854

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/entity/alerting/TransformationLogDAO.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/entity/alerting/TransformationLogDAO.java
@@ -1,19 +1,26 @@
 package io.hyperfoil.tools.horreum.entity.alerting;
 
 
-import jakarta.persistence.ConstraintMode;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 import io.hyperfoil.tools.horreum.entity.PersistentLogDAO;
 import io.hyperfoil.tools.horreum.entity.data.RunDAO;
 import io.hyperfoil.tools.horreum.entity.data.TestDAO;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.id.enhanced.SequenceStyleGenerator;
 
 @Entity(name = "TransformationLog")
 public class TransformationLogDAO extends PersistentLogDAO {
+   @Id
+   @GenericGenerator(
+           name = "transformationlog_id_generator",
+           strategy = "io.hyperfoil.tools.horreum.entity.SeqIdGenerator",
+           parameters = {
+                   @org.hibernate.annotations.Parameter(name = SequenceStyleGenerator.INCREMENT_PARAM, value = "1"),
+           }
+   )
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "transformationlog_id_generator")
+   public Long id;
 
    @ManyToOne(fetch = FetchType.LAZY, optional = false)
    @JoinColumn(name = "testid", foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/entity/report/ReportLogDAO.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/entity/report/ReportLogDAO.java
@@ -1,13 +1,23 @@
 package io.hyperfoil.tools.horreum.entity.report;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 
 import io.hyperfoil.tools.horreum.entity.PersistentLogDAO;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.id.enhanced.SequenceStyleGenerator;
 
 @Entity(name = "ReportLog")
 public class ReportLogDAO extends PersistentLogDAO {
+   @Id
+   @GenericGenerator(
+           name = "reportlog_id_generator",
+           strategy = "io.hyperfoil.tools.horreum.entity.SeqIdGenerator",
+           parameters = {
+                   @org.hibernate.annotations.Parameter(name = SequenceStyleGenerator.INCREMENT_PARAM, value = "1"),
+           }
+   )
+   @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "reportlog_id_generator")
+   public Long id;
    @ManyToOne(optional = false)
    @JoinColumn(name = "report_id")
    TableReportDAO report;
