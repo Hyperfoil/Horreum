1e37436d3aede39e80796744bdf579b71de1fce2
David Butenhof <dbutenho@redhat.com>
2024-01-19 09:04:58 -0500

    Add Action to post message to slack
    The behavior of the action is similar to the existing GitHub actions,
    and largely a cookie-cutter clone. (Some common code could be extracted
    into a base class, but I resisted making unnecessary changes.)
    
    Essentially, it accepts a Slack service token, a Slack channel ID, and
    formatter, and posts the formatted message to the designated Slack
    channel.
    
    On the Slack side, the setup required includes:
    
    1 Define and install a Slack "app" in your Slack workspace, using the
      slack CLI or https://api.slack.com/apps, then generate an app token
      with at least scopes "channel:join" and "chat:write". (Note that a
      workspace may require administrative review and approval to install an
      app.) The token should be a long alphanumeric string starting with
      "xoxb-".
    2 Use the Slack UI (or API) to determine the ID of the channel you want
      to use. (Which looks something like "C00A000A0A".)
    3 Use the https://api.slack.com/methods/conversations.join/test API
      tester with your token and the channel ID to "join" your app to the
      channel so it can post. (There are of course other ways, but this is
      probably the easiest, and provides a quick verification that the token
      and channel ID are good.)
    
    Creating a `slack-channel-message` action requires two "config"
    parameters and one "secrets" parameter:
    
    1 The "channel" conf value
    2 The Horreum "formatter" name
    3 The "token" secret
    
    The Action will always post a "mrkdwn" Slack message using the markdown
    document generated by the Horreum formatter template. It will post using
    the name of the Slack app (e.g., "Horreum Server").
    
    Note that there's some incompatibility between GitHub and Slack markdown
    syntax, specifically for links. Existing Horreum markdown templates for
    GitHub use the `[text](url)` syntax whereas Slack uses `<url|text>`.
    This suggests that Horreum may need to extend the templating classes to
    conditionalize link formatting based on the action context.
    
    For testing (and as an example), this PR proposes a new formatter
    template for the `Test` class, using Slack link syntax.
    
    Finally, I tried to add testing for this using the framework in the
    `ActionServiceTest.java` module. The new test attempts to create a Slack
    action, force it to execute, and validate failure (due to a "bad" fake
    channel and token) by querying the action log. The code fails to find
    any log entries either for the actual test ID or for test "-1", which
    appears to be what `ActionServiceImpl.java` actually uses when logging
    an error. Errors are however visible in the log output.
    
    Resolves issue #838

---
+++ horreum-backend/src/main/resources/templates/slack_from_test.md
@@ -0,0 +1,6 @@
+There is a new test, <{publicUrl}/test/{testId}|{testName}>.
+
+This is the description of the test:
+> {description}
+
+Please <{publicUrl}/changes?test={testNameEncoded}|check it out> and provide an explanatory description.
--- horreum-backend/src/test/java/io/hyperfoil/tools/horreum/svc/ActionServiceTest.java
+++ horreum-backend/src/test/java/io/hyperfoil/tools/horreum/svc/ActionServiceTest.java
@@ -2,27 +2,46 @@ package io.hyperfoil.tools.horreum.svc;
 
 import static io.hyperfoil.tools.horreum.test.TestUtil.eventually;
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
+import java.util.List;
 
+import io.hyperfoil.tools.horreum.action.SlackChannelMessageAction;
 import io.hyperfoil.tools.horreum.api.data.Action;
+import io.hyperfoil.tools.horreum.api.data.ActionLog;
 import io.hyperfoil.tools.horreum.api.data.Test;
 import io.hyperfoil.tools.horreum.bus.AsyncEventChannels;
 import io.hyperfoil.tools.horreum.test.HorreumTestProfile;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.core.HttpHeaders;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import org.jboss.logging.Logger;
+import org.junit.Assert;
 import org.junit.jupiter.api.TestInfo;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import io.hyperfoil.tools.horreum.entity.ActionLogDAO;
+import io.hyperfoil.tools.horreum.entity.PersistentLogDAO;
 import io.hyperfoil.tools.horreum.server.CloseMe;
 import io.hyperfoil.tools.horreum.test.PostgresResource;
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.junit.TestProfile;
 import io.quarkus.test.oidc.server.OidcWiremockTestResource;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 
 @QuarkusTest
 @QuarkusTestResource(PostgresResource.class)
@@ -32,6 +51,10 @@ public class ActionServiceTest extends BaseServiceTest {
 
    @ConfigProperty(name = "quarkus.http.port")
    String port;
+   Logger log = Logger.getLogger(ActionServiceTest.class);
+
+   @Inject
+   ActionServiceImpl actionService;
 
    @org.junit.jupiter.api.Test
    public void testFailingHttp(TestInfo testInfo) {
@@ -63,5 +86,76 @@ public class ActionServiceTest extends BaseServiceTest {
       assertNotNull(action.id);
       assertTrue(action.active);
       given().auth().oauth2(getAdminToken()).delete("/api/action/" + action.id);
+   }
+
+   /**
+    * @param testInfo
+    */
+   @org.junit.jupiter.api.Test
+   public void testMessageBusAction(TestInfo testInfo) {
+      Test test = createTest(createExampleTest(getTestName(testInfo)));
+
+      String channel = "NOTACHANNEL";
+      String token = "BADTOKEN";
+
+      // Build an Action JSON directly because Action DTO serialization will
+      // mask the secrets. (Arguably, since we want to provoke an error here,
+      // the masked "****" value would work just as well, but this code runs
+      // successfully if the channel and token are real.)
+      ObjectMapper mapper = new ObjectMapper();
+      ObjectNode config = mapper.createObjectNode()
+            .put("channel", channel).put("formatter", "testToSlack");
+      ObjectNode secrets = mapper.createObjectNode().put("token", token);
+      ObjectNode action = mapper.createObjectNode()
+            .put("id", -1)
+            .put("event", MessageBusChannels.TEST_NEW.name())
+            .put("type", SlackChannelMessageAction.TYPE_SLACK_MESSAGE)
+            .put("testId", test.id)
+            .put("active", true)
+            .put("runAlways", true);
+
+      // `set` returns a JsonNode, which can't be `set` or `put`, so we do
+      // these outside the chain.
+      action.set("config", config);
+      action.set("secrets", secrets);
+      Action r = jsonRequest().auth().oauth2(getAdminToken())
+            .contentType(ContentType.JSON).body(action)
+            .post("/api/action")
+            .then().statusCode(200)
+            .contentType(ContentType.JSON).extract().body().as(Action.class);
+
+      // send new test to action service
+      actionService.onNewTest(test);
+
+      log.infof("Waiting for action to run");
+      int errors = 0;
+      for (int i = 0; i < 10; i++) {
+         try {
+            Thread.sleep(1000);
+         } catch (InterruptedException e) {
+         }
+
+         // Should these action errors be logged under dummyTest.id? The
+         // ActionServiceImpl handler seems to always log under its argument
+         // value, -1: but I still can't find them.
+         List<ActionLog> logs = jsonRequest().auth().oauth2(getTesterToken())
+               .queryParam("level", PersistentLogDAO.ERROR).get("/api/log/action/-1")
+               .then().statusCode(200)
+               .contentType(ContentType.JSON).extract().body().jsonPath().getList(".", ActionLog.class);
+         log.infof("[%d] Retrieved %d log entries", i, logs.size());
+         for (ActionLog l : logs) {
+            if (l.type == SlackChannelMessageAction.TYPE_SLACK_MESSAGE) {
+               log.infof("Found log for %d: %d, %s, %s: %s", l.testId, l.level, l.event, l.type, l.message);
+               errors++;
+            }
+         }
+      }
+      log.infof("Found %d errors for %s on test %d", errors, SlackChannelMessageAction.TYPE_SLACK_MESSAGE, -1);
+
+      // If only ...
+      // Assert.assertTrue("No slack action errors found", errors > 0);
+
+      // Remove the action. (The test framework will remove the test.)
+      given().auth().oauth2(getAdminToken()).delete("/api/action/" + r.id);
    }
 }
