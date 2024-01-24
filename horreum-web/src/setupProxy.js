d0a46002802e9d9ceba1c3a9c7fe6e0277512b4f
Shivam Sharma <meshivam81@gmail.com>
2024-01-24 10:46:12 +0530

    Migration CRA to VITE
    resolved merge conflicts

--- horreum-web/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable */
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = function(app){
-    const useHttps = process.env.HTTPS
-    const port = useHttps ? 8443 : 8080
-    app.use(createProxyMiddleware('/api',{target: (useHttps ? 'https': 'http') + '://localhost:' + port + '/', secure: false}))
-}
