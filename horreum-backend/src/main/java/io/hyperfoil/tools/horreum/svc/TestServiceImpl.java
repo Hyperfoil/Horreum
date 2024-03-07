1be5d287bf58273d647f4e7fdd2e859fd6f114f5
Ståle Pedersen <stalep@gmail.com>
2024-03-07 18:03:58 +0100

    upgrade to Quarkus 3.8.1, fixes #1244

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/TestServiceImpl.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/TestServiceImpl.java
@@ -211,7 +211,7 @@ public class TestServiceImpl implements TestService {
    TestDAO addAuthenticated(Test dto) {
       TestDAO existing = TestDAO.find("id", dto.id).firstResult();
       if(existing == null)
-         dto.id = null;
+         dto.clearIds();
       TestDAO test = TestMapper.to(dto);
       if (test.notificationsEnabled == null) {
          test.notificationsEnabled = true;
@@ -245,7 +245,7 @@ public class TestServiceImpl implements TestService {
             test.views = Collections.singleton(new ViewDAO("Default", test));
          }
          try {
-            em.merge(test);
+            em.persist(test);
             em.flush();
          } catch (PersistenceException e) {
             if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
