ca5ffd2a163824d1e9802702a49708cb5c414b11
Will Reichert <willr3@gmail.com>
2024-03-18 11:13:53 -0400

    fix#1484 remove final pattern check

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/Util.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/Util.java
@@ -650,12 +650,10 @@ public class Util {
             str=str+"Z";
          }
          //ISO_DATE_TIME
-         if(str.matches("\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}(?:Z|[+\\-]\\d{2}:\\d{2})")){
-            try {
-               return ZonedDateTime.parse(str, DateTimeFormatter.ISO_DATE_TIME).toInstant();
-            } catch (DateTimeParseException e) {
-               e.printStackTrace();
-            }
+         try {
+            return ZonedDateTime.parse(str, DateTimeFormatter.ISO_DATE_TIME).toInstant();
+         } catch (DateTimeParseException e) {
+            e.printStackTrace();
          }
       }
       return null;//nothing matched
