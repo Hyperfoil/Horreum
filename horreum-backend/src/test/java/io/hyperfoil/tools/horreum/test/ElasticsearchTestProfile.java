7e0c075fc00dc5ed0dd98dff2d43676eebdbc133
barreiro <lbbbarreiro@gmail.com>
2024-03-18 18:17:47 +0000

    Remove general use of Elasticsearch dev service

---
+++ horreum-backend/src/test/java/io/hyperfoil/tools/horreum/test/ElasticsearchTestProfile.java
@@ -0,0 +1,14 @@
+package io.hyperfoil.tools.horreum.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ElasticsearchTestProfile extends HorreumTestProfile {
+
+    @Override public Map<String, String> getConfigOverrides() {
+        Map<String, String> configOverrides = new HashMap<>(super.getConfigOverrides());
+        configOverrides.put("quarkus.elasticsearch.devservices.enabled", "true");
+        return configOverrides;
+    }
+
+}
