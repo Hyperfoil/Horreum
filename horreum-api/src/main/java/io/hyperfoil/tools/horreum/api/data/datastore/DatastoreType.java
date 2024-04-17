6f92ebead8a694407ed1eb94f2f727175a1bca7e
Foivos Zakkak <fzakkak@redhat.com>
2024-04-17 17:38:44 +0300

    WIP: Add Collector API datastore

--- horreum-api/src/main/java/io/hyperfoil/tools/horreum/api/data/datastore/DatastoreType.java
+++ horreum-api/src/main/java/io/hyperfoil/tools/horreum/api/data/datastore/DatastoreType.java
@@ -2,7 +2,6 @@ package io.hyperfoil.tools.horreum.api.data.datastore;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.eclipse.microprofile.openapi.annotations.enums.SchemaType;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 
@@ -10,7 +9,8 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
         description = "Type of backend datastore")
 public enum DatastoreType {
     POSTGRES("POSTGRES", new TypeReference<PostgresDatastoreConfig>() {}),
-    ELASTICSEARCH ("ELASTICSEARCH", new TypeReference<ElasticsearchDatastoreConfig>() {});
+    ELASTICSEARCH ("ELASTICSEARCH", new TypeReference<ElasticsearchDatastoreConfig>() {}),
+    COLLECTORAPI("COLLECTORAPI", new TypeReference<CollectorApiDatastoreConfig>() {});
     private static final DatastoreType[] VALUES = values();
 
     private final String name;
