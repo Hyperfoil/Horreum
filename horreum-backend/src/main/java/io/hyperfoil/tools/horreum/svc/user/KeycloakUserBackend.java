9c1a687b2d3b8799da3b67abc6e6152e19f73570
John OHara <johara@redhat.com>
2024-04-23 08:17:51 +0100

    Fix javadoc linting error

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/user/KeycloakUserBackend.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/user/KeycloakUserBackend.java
@@ -32,7 +32,7 @@ import static java.util.stream.Collectors.joining;
 
 /**
  * Implementation of {@link UserBackEnd} using an external Keycloak server.
- * <p/>
+ * 
  * Relies on keycloak-admin-client to manage user information.
  */
 @ApplicationScoped
