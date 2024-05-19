ad8996f7c358b37ec28711002ca8530166c35947
Shivam Sharma <meshivam81@gmail.com>
2024-05-19 06:21:53 +0530

    Use Link instead NavLink

--- horreum-web/src/404.tsx
+++ horreum-web/src/404.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
  
 
 function NotFound() {
@@ -7,7 +7,7 @@ function NotFound() {
     <div className="not-found-container">
       <h1 className="title">404 Not Found</h1>
       <p className="">The page you are looking for does not exist.</p>
-      <NavLink className="homeBtn" to="/">Go to Home</NavLink>
+      <Link to="/" className="homeBtn">Go to Home</Link>
     </div>
        
   );
