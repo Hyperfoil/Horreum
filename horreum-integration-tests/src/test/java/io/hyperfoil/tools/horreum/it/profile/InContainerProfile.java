5f501bf472ab8bd92fb378befa5ee7dda85a060f
barreiro <lbbbarreiro@gmail.com>
2024-06-21 16:15:51 +0100

    [java-client] Fallback to basic authentiticaton when no OIDC token is acquired

--- horreum-integration-tests/src/test/java/io/hyperfoil/tools/horreum/it/profile/InContainerProfile.java
+++ horreum-integration-tests/src/test/java/io/hyperfoil/tools/horreum/it/profile/InContainerProfile.java
@@ -3,15 +3,22 @@ package io.hyperfoil.tools.horreum.it.profile;
 import io.hyperfoil.tools.horreum.it.ItResource;
 import io.quarkus.test.junit.QuarkusTestProfile;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class InContainerProfile implements QuarkusTestProfile {
     @Override
     public Map<String, String> getConfigOverrides() {
-        return Map.of("quarkus.datasource.jdbc.url", "jdbc:postgresql://172.17.0.1:5432/horreum",
-                "quarkus.datasource.migration.jdbc.url", "jdbc:postgresql://172.17.0.1:5432/horreum");
+        return Map.of(
+                "quarkus.datasource.jdbc.url", "jdbc:postgresql://172.17.0.1:5432/horreum",
+                "quarkus.datasource.migration.jdbc.url", "jdbc:postgresql://172.17.0.1:5432/horreum",
+
+                // uncomment the following to test authentication with Horreum instead of Keycloak
+                // "horreum.roles.provider", "database",
+
+                // disable certificate validation (but still require a SSL connection)
+                "quarkus.datasource.jdbc.additional-jdbc-properties.sslmode", "require"
+        );
     }
 
     @Override
