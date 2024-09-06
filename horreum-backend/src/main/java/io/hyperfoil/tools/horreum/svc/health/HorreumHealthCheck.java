b60d32aff506c3e2693eaaadc1fac3bac814abe1
Andrea Lamparelli <a.lamparelli95@gmail.com>
2024-09-06 10:03:57 +0200

    Fix formatting
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/health/HorreumHealthCheck.java
+++ horreum-backend/src/main/java/io/hyperfoil/tools/horreum/svc/health/HorreumHealthCheck.java
@@ -1,6 +1,7 @@
 package io.hyperfoil.tools.horreum.svc.health;
 
 import jakarta.enterprise.context.ApplicationScoped;
+
 import org.eclipse.microprofile.health.HealthCheck;
 import org.eclipse.microprofile.health.HealthCheckResponse;
 import org.eclipse.microprofile.health.Readiness;
@@ -8,8 +9,8 @@ import org.eclipse.microprofile.health.Readiness;
 @Readiness
 @ApplicationScoped
 public class HorreumHealthCheck implements HealthCheck {
-   @Override
-   public HealthCheckResponse call() {
-      return HealthCheckResponse.up("Horreum is ready to accept requests");
-   }
+    @Override
+    public HealthCheckResponse call() {
+        return HealthCheckResponse.up("Horreum is ready to accept requests");
+    }
 }
