b1b620f3421a5e8a1bbfbb6f1345975079cd3138
Andrea Lamparelli <a.lamparelli95@gmail.com>
2025-10-17 16:36:29 +0200

    Return filtering label values as map
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- horreum-api/src/main/java/io/hyperfoil/tools/horreum/api/services/TestService.java
+++ horreum-api/src/main/java/io/hyperfoil/tools/horreum/api/services/TestService.java
@@ -1,6 +1,7 @@
 package io.hyperfoil.tools.horreum.api.services;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import jakarta.validation.constraints.NotNull;
@@ -223,14 +224,24 @@ public interface TestService {
 
     @GET
     @Path("{id}/filteringLabelValues")
-    @Operation(description = "List all unique Label Values for a Test")
+    @Operation(description = "List all unique Label Values for a Test. This api is deprecated, use /filteringLabels instead.")
     @Parameters(value = {
             @Parameter(name = "id", description = "Test ID to retrieve Filtering Label Values for", example = "10"),
     })
     @APIResponses(value = { @APIResponse(responseCode = "200", content = {
             @Content(schema = @Schema(type = SchemaType.ARRAY, implementation = Object.class)) }) })
-    List<ObjectNode> filteringLabelValues(
-            @PathParam("id") int testId);
+    @Deprecated(forRemoval = true, since = "0.19.x")
+    List<ObjectNode> filteringLabelValues(@PathParam("id") int testId);
+
+    @GET
+    @Path("{id}/filteringLabels")
+    @Operation(description = "List all possible values for each filtering label associated to the test")
+    @Parameters(value = {
+            @Parameter(name = "id", description = "Test ID to retrieve Filtering Label Values for", example = "10"),
+    })
+    @APIResponses(value = { @APIResponse(responseCode = "200", content = {
+            @Content(schema = @Schema(type = SchemaType.ARRAY, implementation = Object.class)) }) })
+    Map<String, String[]> filteringLabels(@PathParam("id") int testId);
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
