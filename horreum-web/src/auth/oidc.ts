2fd9c556a342831774a330b48e830657d88f9c44
Andrea Lamparelli <a.lamparelli95@gmail.com>
2025-10-31 16:35:55 +0100

    Use proper realm received by backend
    Signed-off-by: Andrea Lamparelli <a.lamparelli95@gmail.com>

--- horreum-web/src/auth/oidc.ts
+++ horreum-web/src/auth/oidc.ts
@@ -3,7 +3,7 @@ import {User, UserManager, WebStorageStateStore} from "oidc-client-ts";
 
 export const createUserManager = (config: KeycloakConfig): UserManager => {
     return new UserManager({
-        authority: config.url + "/realms/horreum",
+        authority: config.url + "/realms/" + config.realm,
         client_id: config.clientId ?? "",
         redirect_uri: `${window.location.origin}/callback-sso`,
         post_logout_redirect_uri: window.location.origin,
